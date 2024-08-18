5ddb4160321d9123da5581f9322b0e159ef0f773
Arham <aarham948@gmail.com>
2024-08-18 17:24:12 +0100

    added count and contains function

--- Task A/BSTree.cs
+++ Task A/BSTree.cs
@@ -31,6 +31,49 @@ namespace Task_A
             else if (item.CompareTo(tree.Data) > 0) // Compares item with current node data
                 insertItem(item, ref tree.Right); // Inserts item in the right subtree
         }
+        // Public method to count the number of items in the BSTree
+        public void Count(ref int Items)
+        {
+            count(root, ref Items); // Calls the private method to count the items
+        }
+
+        // Private method to recursively count the items in the BSTree
+        private void count(Node<T> tree, ref int Items)
+        {
+            if (tree != null) // Checks if the current node is not null
+            {
+                count(tree.Left, ref Items); // Recursively counts the items in the left subtree
+                Items = Items + 1; // Increments the count for the current node
+                count(tree.Right, ref Items); // Recursively counts the items in the right subtree
+            }
+        }
+
+        // Public method to check if an item is contained in the BSTree
+        public Boolean Contains(ref T item)
+        {
+            return contains(root, ref item); // Calls the private method to check for the item
+        }
+
+        // Private method to recursively check if an item is contained in the BSTree
+        private Boolean contains(Node<T> tree, ref T item)
+        {
+            if (tree != null) // Checks if the current node is not null
+            {
+                if (item.CompareTo(tree.Data) == 0) // Compares the item with the current node's data
+                {
+                    return true; // Returns true if the item is found
+                }
+                else if (item.CompareTo(tree.Data) < 0) // Checks if the item is less than the current node's data
+                {
+                    return contains(tree.Left, ref item); // Recursively checks in the left subtree
+                }
+                else if (item.CompareTo(tree.Data) > 0) // Checks if the item is greater than the current node's data
+                {
+                    return contains(tree.Right, ref item); // Recursively checks in the right subtree
+                }
+            }
+            return false; // Returns false if the item is not found
+        }
 
     }
 }
--- Task A/Program.cs
+++ Task A/Program.cs
@@ -3,90 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task_A;
 
-namespace Task_A
+namespace Task_B
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Initializes a boolean flag to control the loop
-            bool finished = false;
-            // Creates an instance of the Binary Search Tree (BST) to store strings
-            BSTree<string> wordTree = new BSTree<string>();
 
-            // Begins a loop that will run until the user chooses to exit
+            bool finished = false;
+            BSTree<string> wordTree = new BSTree<string>();
             do
             {
-                // Prompts the user to press ENTER and waits for input
                 Console.WriteLine("Press ENTER");
-                Console.ReadKey(); // Waits for the user to press a key
-                Console.Clear(); // Clears the console
-
-                // Displays the menu options to the user
+                Console.ReadKey();
+                Console.Clear();
                 Console.WriteLine("1. Insert a Word");
                 Console.WriteLine("2. Display Tree");
                 Console.WriteLine("3. Display Height");
-                Console.WriteLine("4. Exit");
+                Console.WriteLine("4. Display the total number of words present in the tree");
+                Console.WriteLine("5. Check Word");
+                Console.WriteLine("6. Exit");
                 Console.WriteLine("\n Choose a function");
 
-                // Reads the user's choice
                 string option = Console.ReadLine();
+                int items = 0;
 
-                // Executes the corresponding function based on the user's choice
                 switch (option)
                 {
-                    // Inserts a word into the BST
+
                     case "1":
                         Console.WriteLine("Please insert a word:");
                         wordTree.InsertItem(Console.ReadLine());
                         break;
-
-                    // Calls the traversal method to display the tree in a selected order
                     case "2":
                         traversal();
                         break;
-
-                    // Displays the height of the BST
                     case "3":
                         Console.WriteLine("Height of the Tree: {0}", wordTree.Height());
                         break;
-
-                    // Sets the finished flag to true, exiting the loop
                     case "4":
+                        wordTree.Count(ref items);
+                        Console.WriteLine("Number of items in the Tree: {0}", items);
+                        break;
+                    case "5":
+                        Console.WriteLine("Which word would you like to check?");
+                        string check = Console.ReadLine();
+                        wordTree.Contains(ref check);
+                        Console.WriteLine(wordTree.Contains(ref check));
+                        break;
+                    case "6":
                         finished = true;
                         break;
                 }
-            } while (!finished); // Continues looping until finished is true
-
-            // Defines the traversal method to display the tree
+            } while (!finished);
             void traversal()
             {
-                // Displays the traversal options to the user
                 Console.WriteLine("1. in-order");
                 Console.WriteLine("2. post-order");
                 Console.WriteLine("3. pre-order");
                 Console.WriteLine("\nChoose a tree traversal method:");
 
-                string buffer = ""; // Initializes an empty buffer to store the traversal result
-                string option = Console.ReadLine(); // Reads the user's choice of traversal
+                string buffer = "";
+                string option = Console.ReadLine();
 
-                // Executes the corresponding traversal method based on the user's choice
                 switch (option)
                 {
-                    // Performs in-order traversal
                     case "1":
                         wordTree.InOrder(ref buffer);
                         Console.WriteLine(buffer);
                         break;
-
-                    // Performs post-order traversal
                     case "2":
                         wordTree.PostOrder(ref buffer);
                         Console.WriteLine(buffer);
                         break;
-
-                    // Performs pre-order traversal
                     case "3":
                         wordTree.PreOrder(ref buffer);
                         Console.WriteLine(buffer);
@@ -94,5 +85,6 @@ namespace Task_A
                 }
             }
         }
+
     }
 }
